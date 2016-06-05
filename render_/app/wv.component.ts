2ca120edf97f9baf111895e254e2b0f4dfb43bfd
Erik Portin <erikportin@gmail.com>
2016-06-05 09:08:10 +0200

    sub and pub on render, toggle guest src

--- render_/app/wv.component.ts
+++ render_/app/wv.component.ts
@@ -6,30 +6,41 @@ declare var ppRender: any;
 @Component({
   selector: 'wv',
   template: `
-    <webview 
-             #wv
-             id="wv"
-             src="https://soundcloud.com/jonwayne"
-             src="https://www.bbc.co.uk/radio/player/bbc_6music"
-             src="https://play.spotify.com/browse"
-             style="display:inline-flex; width:640px; height:480px"
-             autosize="on"
-             minwidth="576" minheight="432"
-             plugins
-             allowpopups
-             disablewebsecurity
-    ></webview>
+    {{wvSrc}}
+    <ul>
+        <li><button (click)="setSrc('https://soundcloud.com/jonwayne')">soundcloud</button></li>
+        <li><button (click)="setSrc('https://www.bbc.co.uk/radio/player/bbc_6music')">bbc</button></li>
+        <li><button (click)="setSrc('https://play.spotify.com/browse')">spotify</button></li>
+    </ul>
+    <div *ngIf="wvSrc != ''">
+      <webview 
+               #wv
+               id="wv"
+               style="display:inline-flex; width:640px; height:480px"
+               autosize="on"
+               minwidth="576" minheight="432"
+               plugins
+               allowpopups
+               disablewebsecurity
+      ></webview>    
+    </div>
   `
 })
 
 export class WebviewComponent{
+  _webview:any;
+  wvSrc:String;
+
   @ViewChild('wv') input:ElementRef;
     ngAfterViewInit() {
       console.log(this.input.nativeElement);
-      let _webview = this.input.nativeElement;
-      window.RENDER.setWv(_webview)
+      this._webview = this.input.nativeElement;
 
-        
+      window.RENDER.setWv(this._webview)
+      window.RENDER.subscribe('playpause', function(e){
+        console.log('onPlayPause', e)
+      });
+
       let _loadstart = function() {
         console.log('loading...');
       };
@@ -37,23 +48,30 @@ export class WebviewComponent{
       let _loadstop = function() {
       };
 
-      _webview.addEventListener('did-start-loading', _loadstart);
-      _webview.addEventListener('did-stop-loading', _loadstop);
+      this._webview.addEventListener('did-start-loading', _loadstart);
+      this._webview.addEventListener('did-stop-loading', _loadstop);
 
-      _webview.addEventListener('dom-ready', function(){
-        _webview.openDevTools();
+      this._webview.addEventListener('dom-ready', function(){
+        if(this._webview){
+          this._webview.openDevTools();
+        }
       });
 
-      _webview.addEventListener('console-message', function(e){
+      this._webview.addEventListener('console-message', function(e){
         console.log(e)
       });
 
-      _webview.addEventListener('media-started-playing', function(e){
+      this._webview.addEventListener('media-started-playing', function(e){
         console.log(e)
       });
 
-      _webview.addEventListener('media-paused', function(e){
+      this._webview.addEventListener('media-paused', function(e){
         console.log(e)
       });
     }
+
+  setSrc = (src) =>{
+    this.wvSrc = src;
+    this._webview.src = src;
+  }
 }
